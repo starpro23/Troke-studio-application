4f7bc0eeb1e7566d16602cfac7e5c4fd072665df
starpro23 <23starpro.32@gmail.com>
2025-09-12 11:48:19 +0300

    Updating the configuration files in  project

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,11 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-slate-900 border-t border-slate-800">
       <div className="container mx-auto px-6 py-8 flex flex-col md:flex-row justify-between items-center text-center md:text-left">
-        <p className="text-slate-400 mb-4 md:mb-0">&copy; {new Date().getFullYear()} Troke Studios. All Rights Reserved.</p>
+        <p className="text-slate-400 mb-4 md:mb-0">&copy; {new Date().getFullYear()} Troke Studios. All Rights Reserved. <br />Created and developed by StarTech Group Solutions</p>
         <div className="flex space-x-6">
-            <a href="#" className="text-slate-400 hover:text-cyan-400 transition-colors duration-300"><InstagramIcon /></a>
-            <a href="#" className="text-slate-400 hover:text-cyan-400 transition-colors duration-300"><TwitterIcon /></a>
-            <a href="#" className="text-slate-400 hover:text-cyan-400 transition-colors duration-300"><FacebookIcon /></a>
+            <a href="#" className="text-slate-400 hover:text-cyan-400 transition-colors duration-300"><InstagramIcon /><p>Instagram</p></a>
+            <a href="#" className="text-slate-400 hover:text-cyan-400 transition-colors duration-300"><TwitterIcon /><p>Twitter</p></a>
+            <a href="#" className="text-slate-400 hover:text-cyan-400 transition-colors duration-300"><FacebookIcon /><p>Facebook</p></a>
         </div>
       </div>
     </footer>
